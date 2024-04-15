27bbfe414f32b020880fe4f2fc01af37745fc89c
Administrator <1828742164@qq.com>
2024-04-15 13:54:06 +0800

    update

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,11 +1,11 @@
 package com.ruoyi;
 
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.ip.IpUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.net.InetAddress;
 
 /**
  * 启动程序
@@ -18,10 +18,18 @@ public class RuoYiApplication {
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n");
-        String ip = InetAddress.getLocalHost().getHostAddress();
+        String ip = IpUtils.getHostCardIp();
         String port = context.getEnvironment().getProperty("server.port");
+        String Network;
+        if (StringUtils.isNull(ip)) {
+            Network = "unavailable";
+        } else {
+            Network = "http://" + ip + ":" + port;
+        }
+
         System.out.println("Server running at:\n" +
                 "- Local:   http://localhost:" + port + "\n" +
-                "- Network: http://" + ip + ":" + port);
+                "- Network: " + Network
+        );
     }
 }
